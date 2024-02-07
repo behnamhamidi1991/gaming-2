19d0f74c2b466e60a9dfc75ce05aa4dfd014c1c3
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-07 17:18:33 +0330

    Box shadow

--- src/components/HomeComponents/Productions/Productions.tsx
+++ src/components/HomeComponents/Productions/Productions.tsx
@@ -2,7 +2,30 @@ import "./productions.css";
 import { productions } from "../../../data";
 
 const Productions = () => {
-  return <div className="productions-container"></div>;
+  return (
+    <div className="productions">
+      <div className="header">
+        <h2>Our Top Productions</h2>
+        <p>These are the 8 top games our player buy every season</p>
+      </div>
+
+      <div className="prodictions-container">
+        {productions.map((item) => (
+          <div key={item.id} className="production">
+            <div className="production-image-container">
+              <img src={item.image} alt={item.title} />
+            </div>
+            <div className="production-content">
+              <h3 className="production-content-title">{item.title}</h3>
+              <p className="production-content-text">
+                {item.text.substring(1, 190)} ...
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Productions;
--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
+// import Footer from "../components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
@@ -11,7 +11,7 @@ const AppLayout = () => {
       <main>
         <Outlet />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
