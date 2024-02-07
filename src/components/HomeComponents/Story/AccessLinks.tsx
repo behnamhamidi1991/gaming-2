a0949d0b69e2ad214c7b0cff0374d108f4db1551
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-08 02:14:55 +0330

    Comment

--- src/components/HomeComponents/Story/AccessLinks.tsx
+++ src/components/HomeComponents/Story/AccessLinks.tsx
@@ -1,4 +1,9 @@
 import "./access.css";
+import { FaUser } from "react-icons/fa";
+import { MdOutlineWorkspacePremium } from "react-icons/md";
+import { TbHelpSquareFilled } from "react-icons/tb";
+import { CiStreamOn } from "react-icons/ci";
+import { FaTrophy } from "react-icons/fa6";
 
 const AccessLinks = () => {
   return <div></div>;
