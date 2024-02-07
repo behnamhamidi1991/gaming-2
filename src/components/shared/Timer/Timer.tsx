9e8283b3cb80dc2b7415a32feb66ae166e33479f
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-07 21:02:20 +0330

    Commit

---
+++ src/components/shared/Timer/Timer.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const Timer = () => {
+  const [days, setDays] = useState<number>(0);
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
+
+  const deadline = "August, 25, 2024";
+
+  const getTime = () => {
+    const time = Date.parse(deadline) - Date.now();
+
+    setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+    setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+    setMinutes(Math.floor((time / 1000 / 60) % 60));
+    setSeconds(Math.floor((time / 1000) % 60));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => getTime(deadline), 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div className="timer" style={{ display: "flex", gap: "20px" }}></div>;
+};
+
+export default Timer;
