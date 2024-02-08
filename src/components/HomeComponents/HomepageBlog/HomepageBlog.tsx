52943a6385d8f59f190a59bd6c7b098b3de86894
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-08 16:25:03 +0330

    Blog is done, responsive begins

--- src/components/HomeComponents/HomepageBlog/HomepageBlog.tsx
+++ src/components/HomeComponents/HomepageBlog/HomepageBlog.tsx
@@ -1,15 +1,19 @@
 import "./homepageblog.css";
 import { blogPosts } from "../../../blogData";
+import { FaUser } from "react-icons/fa";
+import { CgCalendarDates } from "react-icons/cg";
 
 const HomepageBlog = () => {
   return (
     <div className="homepage-blog">
       <div className="homepage-blog-header">
         <h2>Blog Page | News</h2>
-        <p>
-          Here you find the most interesting posts and news about the gaming
-          world!
-        </p>
+        <div className="ptext-wrapper">
+          <p className="animation-text">
+            Here you find the most interesting posts and news about the gaming
+            world!
+          </p>
+        </div>
       </div>
 
       <div className="homepage-blog-container">
@@ -18,13 +22,30 @@ const HomepageBlog = () => {
             <img src={post.image} alt={post.title} />
             <div className="blog-content">
               <h3>{post.title}</h3>
-              <p>{post.text.substring(1, 223)} ...</p>
+              <p>
+                {post.id === "1"
+                  ? post.text.substring(1, 550)
+                  : post.text.substring(1, 223)}{" "}
+                ...
+              </p>
               <div className="post-info">
-                <span>{post.date}</span>
-                <span>{post.writer}</span>
-                <span>{post.views}</span>
+                <span>
+                  {" "}
+                  <CgCalendarDates />
+                  {post.date}
+                </span>
+                <span>
+                  <FaUser />
+                  {post.writer}
+                </span>
+                <span>Views: {post.views}</span>
               </div>
-              <div className="post-hashtags"></div>
+              <div className="post-hashtags">
+                {post.hashtags.map((hashtag) => (
+                  <span># {hashtag}</span>
+                ))}
+              </div>
+              <button className="blog-readmore-btn">Read More</button>
             </div>
           </div>
         ))}
