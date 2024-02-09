3e702b355ce40b380c119b166e559c1b76312690
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-09 22:13:09 +0330

    Comment section

--- src/components/shared/Comments/Comments.tsx
+++ src/components/shared/Comments/Comments.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import "./comments.css";
 
 const Comments = () => {
-  return <div></div>;
+  return (
+    <div className="comments">
+      <div className="comments-top">
+        <h4>Comments</h4>
+        <p>
+          Share your feedback with us about this post and tell us what you think{" "}
+        </p>
+      </div>
+      <div className="comments-main">
+        <form>
+          <input type="text" placeholder="Enter your name ..." />
+          <input type="email" placeholder="Enter your email ..." />
+          <textarea placeholder="Write your comment here ..." />
+          <button>Submit</button>
+        </form>
+      </div>
+      <div className="comments-bottom"></div>
+    </div>
+  );
 };
 
 export default Comments;
--- src/pages/SingleProduction/SingleProduction.tsx
+++ src/pages/SingleProduction/SingleProduction.tsx
@@ -1,7 +1,10 @@
 import { productions } from "../../data";
 import { useParams } from "react-router-dom";
+import { FaCartShopping } from "react-icons/fa6";
+
 import "./singleproduction.css";
 import Sidebar from "../../components/shared/Sidebar/Sidebar";
+import Comments from "../../components/shared/Comments/Comments";
 
 const SingleProduction = () => {
   const { id } = useParams();
@@ -15,10 +18,32 @@ const SingleProduction = () => {
           {/* LEFT */}
           <div className="production-image-content">
             <img src={newProduct?.image} alt={newProduct?.title} />
-            <h1>{newProduct?.title}</h1>
-            <p>{newProduct?.text}</p>
+            <div className="production-content-box">
+              <h1>{newProduct?.title}</h1>
+              <p>{newProduct?.text}</p>
+              <div className="production-content-info-box">
+                <span>Price: ${newProduct?.price}</span>
+                <span>
+                  {newProduct?.offer === true
+                    ? "Off-price: " + newProduct?.offPrice
+                    : null}
+                </span>
+                <span>
+                  {newProduct?.offer === true
+                    ? "Discount: " + newProduct?.percentage
+                    : null}
+                </span>
+                <span>Rate: {newProduct?.rate}</span>
+              </div>
+              <button className="single-production-add-btn">
+                <FaCartShopping />
+                Add To Cart
+              </button>
+            </div>
           </div>
-          {/* LEFT */}
+          <div className="single-production-comments">
+            <Comments />
+          </div>
         </div>
         <div className="single-production-right">
           <Sidebar />
