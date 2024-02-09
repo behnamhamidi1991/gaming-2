d04f5c53ca03b9909cc01f5202fe553a626c5c75
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-10 02:33:50 +0330

    Final commit

--- src/components/HomeComponents/Battle/Battle.tsx
+++ src/components/HomeComponents/Battle/Battle.tsx
@@ -12,9 +12,9 @@ const Battle = () => {
       className="battle"
       ref={ref}
       style={{
-        transform: isInView ? "none" : "translateY(200px)",
+        transform: isInView ? "none" : "translateX(-200px)",
         opacity: isInView ? 1 : 0,
-        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+        transition: "all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s",
       }}
     >
       <div className="header">
