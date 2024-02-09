545dd8d75e9b1e500fe851d4cce3a6f582d06e57
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-09 14:12:41 +0330

    price data

--- src/components/HomeComponents/Prices/Prices.tsx
+++ src/components/HomeComponents/Prices/Prices.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import "./prices.css";
 
 const Prices = () => {
-  return <div></div>;
+  return (
+    <div className="price-section">
+      <div className="price-header"></div>
+
+      <div className="price-container">
+        <div className="price-box"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Prices;
