00eb9642fa8855ad30be48ee5e96db05c3af5977
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-10 01:50:44 +0330

    Commit before framer

--- src/components/shared/UserSidebar/UserSidebar.tsx
+++ src/components/shared/UserSidebar/UserSidebar.tsx
@@ -1,7 +1,43 @@
 import "./usersidebar.css";
+import { users } from "../../../userData";
 
 const UserSidebar = () => {
-  return <div>Sidebar</div>;
+  const userLogin = users.filter((item) => item.id === "3");
+
+  return (
+    <div className="user-sidebar">
+      <div className="user-sidebar-user">
+        {userLogin.map((user) => (
+          <div key={user.id} className="user-sidebar-user-box">
+            <img src={user.profileImage} alt="user-profile" />
+            <div className="user-sidebar-user-content">
+              <p>Name: {user.name}</p>
+              <p>
+                Status:{" "}
+                <span style={{ color: "rgb(16, 190, 126)", fontWeight: "700" }}>
+                  Online
+                </span>
+              </p>
+              <p>Followers: {user.followers}</p>
+              <p>Following: {user.following}</p>
+              <p>
+                Account:{" "}
+                <span style={{ color: "#3c62df", fontWeight: "700" }}>
+                  VIP Account
+                </span>
+              </p>
+              <p>Country: {user.country}</p>
+              <p>City: {user.city}</p>
+            </div>
+            <div className="user-sidebar-btn-container">
+              <button className="user-sidebar-logout-btn">Logout</button>
+              <button className="user-sidebar-settings-btn">Settings</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default UserSidebar;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,11 @@
 import "./dashboard.css";
 import UserSidebar from "../../components/shared/UserSidebar/UserSidebar";
+import { FaCartPlus } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa";
+import { HiMiniUserGroup } from "react-icons/hi2";
+import { FaServer } from "react-icons/fa";
+import { RiVipFill } from "react-icons/ri";
+import { BsAwardFill } from "react-icons/bs";
 
 const Dashboard = () => {
   return (
@@ -9,7 +15,38 @@ const Dashboard = () => {
         <p>Manage your account through this page</p>
       </div>
       <div className="dashboard-content">
-        <div className="dashboard-content-left">Test</div>
+        <div className="dashboard-content-left">
+          <div className="dashboard-item">
+            <FaCartPlus className="dashboard-item-icon" />
+            <h4>Your Orders</h4>
+            <p>0 Orders for now</p>
+          </div>
+          <div className="dashboard-item">
+            <FaUser className="dashboard-item-icon" />
+            <h4>Followers</h4>
+            <p>12549 people follow you</p>
+          </div>
+          <div className="dashboard-item">
+            <HiMiniUserGroup className="dashboard-item-icon" />
+            <h4>Following</h4>
+            <p>You follow 8465 people</p>
+          </div>
+          <div className="dashboard-item">
+            <FaServer className="dashboard-item-icon" />
+            <h4>Following</h4>
+            <p>You follow 8465 people</p>
+          </div>
+          <div className="dashboard-item">
+            <RiVipFill className="dashboard-item-icon" />
+            <h4>Your VIP Card</h4>
+            <p>Click to change options</p>
+          </div>
+          <div className="dashboard-item">
+            <BsAwardFill className="dashboard-item-icon" />
+            <h4>Awards</h4>
+            <p>FIFA 2023 - Vienna Competition</p>
+          </div>
+        </div>
         <div className="dashboard-content-right">
           <UserSidebar />
         </div>
