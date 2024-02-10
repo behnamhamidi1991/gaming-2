951583e3104ae8a81b5bc2e87b3c62d9befdd7a1
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-10 18:56:07 +0330

    Commit without context

--- src/Context/gameContext.tsx
+++ src/Context/gameContext.tsx
@@ -16,17 +16,36 @@ interface ProductionInterface {
 
 export const GameContext = createContext({
   product: [] as ProductionInterface[],
+  cart: [] as ProductionInterface[],
 });
 
 export const GameProvider = ({ children }: any) => {
   const [product, setProducts] = useState(productions);
+  const [cart, setCart] = useState([]);
 
-  console.log(product);
+  const handleClick = (item) => {
+    let isPresent = false;
+    cart.forEach((game) => {
+      if (item.id === game.id) isPresent = true;
+    });
+
+    if (isPresent) {
+      window.alert(
+        "This game already exists in your shopping cart! Go to the cart page to handle the process."
+      );
+      return;
+    } else {
+      setCart([...cart, item]);
+    }
+  };
 
   return (
     <GameContext.Provider
       value={{
         product,
+        cart,
+        setProducts,
+        handleClick,
       }}
     >
       {children}
--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -2,21 +2,18 @@ import { useState } from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import { Outlet } from "react-router-dom";
-import { GameProvider } from "../Context/gameContext";
 
 const AppLayout = () => {
   const [darkTheme, setDarkTheme] = useState<boolean>(false);
   return (
-    <GameProvider>
-      <div className={darkTheme ? "main dark" : "main light"}>
-        <Header darkTheme={setDarkTheme} theme={darkTheme} />
+    <div className={darkTheme ? "main dark" : "main light"}>
+      <Header darkTheme={setDarkTheme} theme={darkTheme} />
 
-        <main>
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </GameProvider>
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
